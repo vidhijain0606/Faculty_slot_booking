f13b92e690e237fa5e65a5431b278cc94f58ce01
Vidhi <vidhijj0606@gmail.com>
2025-12-17 12:16:41 +0530

    Redesigned booking options

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/lib/auth";
 import { ProtectedRoute } from "@/components/ProtectedRoute";
+
 import Welcome from "./pages/Welcome";
 import Index from "./pages/Index";
 import Auth from "./pages/Auth";
@@ -19,107 +20,111 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <AuthProvider>
-          <Routes>
-            {/* Public Routes */}
-            <Route path="/" element={<Auth />} />
-            <Route path="/auth" element={<Auth />} />
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
 
-            {/* Protected Routes */}
-            <Route 
-              path="/welcome" 
-              element={
-                <ProtectedRoute allowedRoles={['faculty', 'admin']}>
-                  <Welcome />
-                </ProtectedRoute>
-              } 
-            />
-            
-            <Route 
-              path="/dashboard" 
-              element={
-                <ProtectedRoute allowedRoles={['faculty']}>
-                  <Index />
-                </ProtectedRoute>
-              } 
-            />
-            
-            <Route
-              path="/faculty"
-              element={
-                <ProtectedRoute allowedRoles={['faculty']}>
-                  <FacultyDashboard />
-                </ProtectedRoute>
-              }
-            />
-            
-            <Route
-              path="/admin"
-              element={
-                <ProtectedRoute allowedRoles={['admin']}>
-                  <AdminDashboard />
-                </ProtectedRoute>
-              }
-            />
-            
-            <Route
-              path="/book/:slotId"
-              element={
-                <ProtectedRoute allowedRoles={['faculty']}>
-                  <BookingPage />
-                </ProtectedRoute>
-              }
-            />
-            
-            <Route
-              path="/upload-document"
-              element={
-                <ProtectedRoute allowedRoles={['admin']}>
-                  <UploadDocument />
-                </ProtectedRoute>
-              }
-            />
+        <BrowserRouter>
+          <AuthProvider>
+            <Routes>
+              {/* Public */}
+              <Route path="/" element={<Auth />} />
+              <Route path="/auth" element={<Auth />} />
 
-            <Route
-              path="/view-documents"
-              element={
-                <ProtectedRoute allowedRoles={['faculty', 'admin']}>
-                  <ViewDocuments />
-                </ProtectedRoute>
-              }
-            />
-            
-            <Route
-              path="/link-document-1"
-              element={
-                <ProtectedRoute allowedRoles={['faculty']}>
-                  <LinkDocument1 />
-                </ProtectedRoute>
-              }
-            />
-            
-            <Route
-              path="/link-document-2"
-              element={
-                <ProtectedRoute allowedRoles={['faculty']}>
-                  <LinkDocument2 />
-                </ProtectedRoute>
-              }
-            />
-            
-            {/* Catch-all 404 Route - MUST BE LAST */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </AuthProvider>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+              {/* Welcome */}
+              <Route
+                path="/welcome"
+                element={
+                  <ProtectedRoute allowedRoles={["faculty", "admin"]}>
+                    <Welcome />
+                  </ProtectedRoute>
+                }
+              />
 
-export default App;
+              {/* Faculty */}
+              <Route
+                path="/dashboard"
+                element={
+                  <ProtectedRoute allowedRoles={["faculty"]}>
+                    <Index />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route
+                path="/faculty"
+                element={
+                  <ProtectedRoute allowedRoles={["faculty"]}>
+                    <FacultyDashboard />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route
+                path="/book/:slotId"
+                element={
+                  <ProtectedRoute allowedRoles={["faculty"]}>
+                    <BookingPage />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route
+                path="/link-document-1"
+                element={
+                  <ProtectedRoute allowedRoles={["faculty"]}>
+                    <LinkDocument1 />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route
+                path="/link-document-2"
+                element={
+                  <ProtectedRoute allowedRoles={["faculty"]}>
+                    <LinkDocument2 />
+                  </ProtectedRoute>
+                }
+              />
+
+              {/* Admin */}
+              <Route
+                path="/admin"
+                element={
+                  <ProtectedRoute allowedRoles={["admin"]}>
+                    <AdminDashboard />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route
+                path="/upload-document"
+                element={
+                  <ProtectedRoute allowedRoles={["admin"]}>
+                    <UploadDocument />
+                  </ProtectedRoute>
+                }
+              />
+
+              {/* Shared */}
+              <Route
+                path="/view-documents"
+                element={
+                  <ProtectedRoute allowedRoles={["faculty", "admin"]}>
+                    <ViewDocuments />
+                  </ProtectedRoute>
+                }
+              />
+
+              {/* 404 */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </AuthProvider>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
